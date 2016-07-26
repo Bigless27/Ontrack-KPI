912cd795f929ed3b867f6830a89b573b9782bfb3
Bigless27 <lessonryan@gmail.com>
2016-07-26 10:29:15 -0500

    set up jwt

--- onTrack.API/server/auth/authController.js
+++ onTrack.API/server/auth/authController.js
@@ -1,30 +1,10 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var webToken = require('jsonwebtoken');
+var User = require('../api/user/userModel');
+var signToken = require('./auth').signToken;
 
-var authRouter = require('express').Router;
-
-var User = require('../schemas/user');
-
-
-authRouter.post('/authenticate', function(req, res) {
-    User.findOne({ email: req.body.email },
-        function(err, user) {
-            if(err) throw err;
-            if(!user) {
-                res.json({ success: false, message: 'Authentication failed.  User not found.' });
-            } else if(user) {
-                if(user.password != req.body.password) {
-                    res.json({ success: false, message: 'Authentication failed.  Incorrect password.' });                    
-                } else {
-                    var token = webToken.sign(user, app.get('tokenSecret'), { expiresInMinutes: 1440 });
-
-                    res.json({
-                        success: true,
-                        message: 'Authenticated',
-                        token: token
-                    });
-                }
-            }
-        });
-});
+exports.authenticat = function(req, res, next) {
+    // req.user will be there from the middleware
+    // varify user. Then we can just create a token
+    // and send it back for the client to consume
+    var token = signToken(req.user._id);
+    res.json({token: token})
+};
--- onTrack.API/server/auth/authRoutes.js
+++ onTrack.API/server/auth/authRoutes.js
@@ -1,18 +1,28 @@
-var authRouter = require('express').Router();
-var webToken = require('jsonwebtoken');
-var authController = require('../controllers/auth');
+var router = require('express').Router();
+var verifyUser = require('./auth').verifyUser;
+var controller = require('./controller');
 
-authRouter.post('/api/authenticate', function(req, res) {
-    var username = req.body.email;
-    var password = req.body.password;
-    var token = webToken.sign(user, app.get('tokenSecret'), { expiresInMinutes: 1440 });
-
-    res.json({
-        success: true,
-        message: 'this is not secure',
-        token: token
-    });
-});
+// before we send vack a jwt, lets check
+// the password and username mathch what is in the DB
+router.post('/authenticate', verifyUser(), controller.signin)
 
 
-module.exports = authRouter;
+
+// var authRouter = require('express').Router();
+// var webToken = require('jsonwebtoken');
+// var authController = require('../controllers/auth');
+
+// authRouter.post('/api/authenticate', function(req, res) {
+//     var username = req.body.email;
+//     var password = req.body.password;
+//     var token = webToken.sign(user, app.get('tokenSecret'), { expiresInMinutes: 1440 });
+
+//     res.json({
+//         success: true,
+//         message: 'this is not secure',
+//         token: token
+//     });
+// });
+
+
+// module.exports = authRouter;
