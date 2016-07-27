cac06b03c882b2b0faff84f6fb5da157bc9b2976
Bigless27 <lessonryan@gmail.com>
2016-07-27 12:25:05 -0500

    update client schema

--- onTrack.API/server/api/client/clientModel.js
+++ onTrack.API/server/api/client/clientModel.js
@@ -1,14 +1,31 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var ContactPhone = new Schema({
+    clientId: {type: Schema.Types.ObjectId, ref: 'client'},
+    phoneNumber: {type: String, required: true, unique: true},
+    phoneType: {type: String, required: true},
+    isPrimary: {type: Boolean, default: true}
+});
+
+var ContactEmail: newSchema({
+	clientId: {type: Schema.Types.ObjectId, ref: client},
+	emailAddress: {type: String, require: true},
+	emailType: {type: String, require: true},
+	isPrimary: {type: Boolean, default: true}
+})
+
+
 var ClientSchema = new Schema({
-	Name: {type: String, required: true, index: true},
-	AccountStatus: {type: String, required: true},
-	BillingStatus: {type: String, required: true},
-	KPIS: [{type: Schema.Types.ObjectId, ref: 'kpi'}],
-	Users: [{type: Schema.Types.ObjectId, ref: 'user'}],
-	StartDate: {type: Date},
-	EndDate: {type: Date}
+	name: {type: String, required: true, index: true},
+	accountStatus: {type: String, required: true},
+	billingStatus: {type: String, required: true},
+	kPIS: [{type: Schema.Types.ObjectId, ref: 'kpi'}],
+	contactPhone: [ContactPhone],
+	contactEmail: [ContactEmail],
+	users: [{type: Schema.Types.ObjectId, ref: 'user'}],
+	startDate: {type: Date},
+	endDate: {type: Date}
 })
 
 module.exports = mongoose.model('client', ClientSchema)
--- onTrack.API/server/api/kpi/kpiModel.js
+++ onTrack.API/server/api/kpi/kpiModel.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var KpiSchema = new Schema({
-    ClientId: { type: Schema.Types.ObjectId, ref: 'user'}, // what is this?
-    Name: { type: String, unique: true, required: true, index: true },
-    Type: { type: String },
-    Value: { type: String }
+    clientId: { type: Schema.Types.ObjectId, ref: 'client'}, // what is this?
+    name: { type: String, unique: true, required: true, index: true },
+    type: { type: String },
+    value: { type: String }
 });
 
 module.exports = mongoose.model('kpi', KpiSchema);
