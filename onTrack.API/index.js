4366ce14c6df6539ab51ae1f1ccb72fec6c6d5d7
Bigless27 <lessonryan@gmail.com>
2016-08-08 14:43:59 -0500

    set up clusters for production

--- onTrack.API/index.js
+++ onTrack.API/index.js
@@ -11,33 +11,7 @@ var logger = require('./server/util/logger');
 var cluster = require('cluster');
 var http = require('http');
 
-if(cluster.isMaster) {
-    var numWorkers = require('os').cpus().length;
+var server = app.listen(config.port, function() {
+    logger.log('listening on http://localhost:' + config.port);
+})
 
-    logger.log('Master cluster setting up ' + numWorkers + ' workers...');
-
-    for(var i = 0; i < numWorkers; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('online', function(worker) {
-        logger.log('Worker ' + worker.process.pid + ' is online');
-    });
-
-    cluster.on('exit', function(worker, code, signal) {
-        logger.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-        logger.log('Starting a new worker');
-        cluster.fork();
-    });
-} else {
-    var app = require('express')();
-    app.all('/*', function(req, res) {res.send('process ' + process.pid + ' says hello!').end();})
-
-    var server = app.listen(config.port, function() {
-        logger.log('Process ' + process.pid + ' is listening to all incoming requests');
-    });
-}
-
-
-// app.listen(config.port);
-// logger.log('listening on http://localhost:' + config.port);
