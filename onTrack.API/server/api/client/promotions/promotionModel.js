6b978bc1adc787dfae9bdb4de1a098f1817b81cb
Bigless27 <lessonryan@gmail.com>
2016-08-16 14:50:21 -0500

    post hook to client association and populate promotions

--- onTrack.API/server/api/client/promotions/promotionModel.js
+++ onTrack.API/server/api/client/promotions/promotionModel.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Client = require('../clientModel')
 
 var PromotionSchema = new Schema({
 	name: {type: String, required: true, index: true},
@@ -18,4 +19,29 @@ PromotionSchema.pre('remove', function(next) {
 	console.log('deleting associations')
 	this.model('client').remove({promotions: this.id}, next)
 })
+
+PromotionSchema.post('save', function(doc) {
+	Client.findById(doc.clientId)
+		.then(function(client) {
+
+			if(!client) {
+				console.log('err')
+			}
+
+			user.clientId.push(doc._id)
+			client.promotions.push(doc._id)
+			user.save(function(err){
+				if(err) {
+					console.log(err)
+				} else {
+					console.log('saved')
+				}
+			})
+		}, function(err) {
+			return err
+	})
+})
+
+
+
 module.exports = mongoose.model('promotion', PromotionSchema)
