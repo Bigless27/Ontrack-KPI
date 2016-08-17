44166e8bdba8025e9792aeaff037a3f6d7f0db20
Bigless27 <lessonryan@gmail.com>
2016-08-17 11:46:17 -0500

    update remove method for settings delete

--- onTrack.API/server/api/client/settings/settingsModel.js
+++ onTrack.API/server/api/client/settings/settingsModel.js
@@ -9,9 +9,26 @@ var SettingsSchema = new Schema({
 	description: {type: String, required: true}
 });
 
-SettingsSchema.pre('remove', function(next) {
-	console.log('deleting association');
-	this.model('client').remove({settings: this.id}, next)
+SettingsSchema.post('remove', function(next) {
+	Client.findById(doc.clientId)
+		.then(function(client) {
+			if(!client) {
+				console.log('association not deleted')
+			} else {
+				update = client
+				
+				update.settings.splice(update.settings.indexOf(doc._id),1);
+				_.merge(client, update)
+
+				client.save(function(err, saved) {
+					if (err) {
+						console.log('not saved')
+					} else {
+						console.log('association deleted and saved')
+					}
+				})
+			}
+		})
 })
 
 
