f78fa2a2654a4db4777df181392837ae521c172c
Bigless27 <lessonryan@gmail.com>
2016-12-02 13:08:05 -0600

    delete activity router from server

--- onTrack.API/server/api/api.js
+++ onTrack.API/server/api/api.js
@@ -4,7 +4,6 @@ router.use('/teams', require('./teams/teamRoutes'));
 router.use('/users', require('./users/userRoutes'));
 router.use('/type-settings', require('./settings/type-settings/settingsRoutes'))
 router.use('/progress-settings', require('./settings/progress-settings/settingsRoutes'))
-router.use('/activity', require('./activity/activityRoutes'))
 
 
 module.exports = router;
